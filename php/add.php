010851066d74bfd6210dd0459d09d4df15fcdc15
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2023-01-06 15:54:56 +0200

    Add files via upload

--- php/add.php
+++ php/add.php
@@ -1,8 +1,25 @@
 <?php
+$msg = ""
 if(isset($_POST["save"])) {
     require 'database.php'
     $username = $_POST["username"];
-    $password = password_hash($_POST["password"]);
+    $password = PASSWORD_HASH($_POST["password"] PASSWORD_DEFAULT);
     $wbsite = $_POST["webstie"];
+    // check if all the fields are empty
+    if(empty($username) || empty($password)|| empty($webstie)) {
+        msg = "Fields are empty. Please fill them in";
+        echo $msg;
+    } elsif(empty($susername) ) {
+        $msg = "username field is empty. Please fill it in";
+        echo $msg
+
+    } elseif(empty($password)) {
+        $msg = "Password field is empty. Please fill it in";
+        echo $msg;
+    } else {
+        $stnmt = $conn->prepare("INSERT INTO  password_info( username, password, website) VALUES(?,?,?);" );
+        $stnmt->bind_param("sss", $username, $password, $website);
+        
+    }
 
 }
